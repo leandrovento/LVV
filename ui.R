9f36db835a7aa8b422c51cd5d9f11933dbc42b4f
Leandro <leandro.vigari@hotmail.com>
2019-09-22 09:59:33 -0300

    novo

--- ui.R
+++ ui.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(shinyWidgets)
 
 # Define UI for application that draws a histogram
 shinyUI(
@@ -17,10 +18,53 @@ shinyUI(
     dashboardPagePlus(
         header = dashboardHeaderPlus(
             enable_rightsidebar = TRUE,
-            rightSidebarIcon = "gears"
+            rightSidebarIcon = "gears",
+            left_menu = tagList(
+                dropdownBlock(
+                    id = "mydropdown",
+                    title = "Dropdown 1",
+                    icon = icon("sliders"),
+                    sliderInput(
+                        inputId = "n",
+                        label = "Number of observations",
+                        min = 10, max = 100, value = 30
+                    ),
+                    prettyToggle(
+                        inputId = "na",
+                        label_on = "NAs keeped",
+                        label_off = "NAs removed",
+                        icon_on = icon("check"),
+                        icon_off = icon("remove")
+                    )
+                ),
+                dropdownBlock(
+                    id = "mydropdown2",
+                    title = "Dropdown 2",
+                    icon = icon("sliders"),
+                    prettySwitch(
+                        inputId = "switch4",
+                        label = "Fill switch with status:",
+                        fill = TRUE,
+                        status = "primary"
+                    ),
+                    prettyCheckboxGroup(
+                        inputId = "checkgroup2",
+                        label = "Click me!",
+                        thick = TRUE,
+                        choices = c("Click me !", "Me !", "Or me !"),
+                        animation = "pulse",
+                        status = "info"
+                    )
+                )
+            )
         ),
         sidebar = dashboardSidebar(),
-        body = dashboardBody(),
+        body = dashboardBody(
+            
+            box(width = 4, height = "300px",
+            loadingState()
+            )
+        ),
         rightsidebar = rightSidebar(
             background = "dark",
             rightSidebarTabContent(
